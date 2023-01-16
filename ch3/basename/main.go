c192cbf3e58c5aab18cf12509e95964156c98485
beiyougufen <2270423705@qq.com>
2023-01-17 03:53:56 +0800

    ch3 code samples

---
+++ ch3/basename/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		fmt.Println(basename1(input.Text()))
+		fmt.Println(basename2(input.Text()))
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
+
+// basename removes directory components and a .suffix.
+// e.g., a => a, a.go => a, a/b/c.go => c, a/b.c.go => b.c
+func basename2(s string) string {
+	slash := strings.LastIndex(s, "/") // -1 if "/" not found
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
+
+// basename removes directory components and a .suffix.
+// e.g., a => a, a.go => a, a/b/c.go => c, a/b.c.go => b.c
+func basename1(s string) string {
+	// Discard last '/' and everything before.
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+	// Preserve everything before last '.'.
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+	return s
+}
---
+++ ch3/ch3.md
@@ -0,0 +1,39 @@
+# 基础数据类型
+
+## 1. 整型
+
+## 2. 浮点数
+
+surface/main.go
+
+## 3. 复数
+
+mandelbrot/main.go
+
+## 4. 布尔型
+
+## 5. 字符串
+
+### 5.1  字符串面值
+
+### 5.2 Unicode
+
+### 5.3 UTF-8
+
+### 5.4 字符串和Byte切片
+
+basename/main.go
+
+comma/main.go
+
+printints/main.go
+
+### 5.5 字符串和数字的转换
+
+## 6. 常量
+
+### 6.1 iota 常量生成器
+
+netflag/main.go
+
+### 6.2 无类型常量
